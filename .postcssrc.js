cb9df459706702f3d816b054687169afc9ad9e35
小渔儿 <yujingpingbo@163.com>
2021-06-23 10:59:17 +0800

    修改模板

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,7 +1,15 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "autoprefixer": {}
-  }
+  	"plugins": {
+    	"autoprefixer": { 
+    		overrideBrowserslist: [ 
+    			"Android 4.1", 
+    			"iOS 7.1", 
+    			"Chrome > 24", 
+    			"ff > 3.5", 
+    			"ie >= 8" 
+    		] 
+    	}
+  	}
 }
